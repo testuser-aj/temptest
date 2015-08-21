dad61c90e58891f0517a2d4db189e4fd90efd2e7
Ajay Kannan <ajaykannan@google.com>
2015-08-21 16:26:44 -0700

    change from readonly

--- utilities/after_success.sh
+++ utilities/after_success.sh
@@ -7,9 +7,11 @@ echo "Travis branch:       " ${TRAVIS_BRANCH}
 echo "Travis pull request: " ${TRAVIS_PULL_REQUEST}
 echo "Travis JDK version:  " ${TRAVIS_JDK_VERSION}
 if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" -a "${TRAVIS_BRANCH}" == "master" -a "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+    echo `git config --get remote.origin.url`
     git config --global user.email "Travis CI"
     git config --global user.name "ajay.kannan.15@dartmouth.edu"
-    git clone -b gh-pages https://${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD}@github.com/testuser-aj/temptest.git
+    #git clone -b gh-pages https://${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD}@github.com/testuser-aj/temptest.git
+    git clone -b gh-pages `git config --get remote.origin.url` .
     git remote set-url origin git@github.com:${CI_DEPLOY_USERNAME}/temptest.git
     SITE_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
     SITE_VERSION="$(cut -d '-' -f 1 <<< "$SITE_VERSION")"
