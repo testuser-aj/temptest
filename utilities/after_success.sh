8e969de60a380cc4ec00a496eccf1cdd8d8f2027
Ajay Kannan <ajaykannan@google.com>
2015-08-21 17:53:28 -0700

    ?

--- utilities/after_success.sh
+++ utilities/after_success.sh
@@ -11,11 +11,12 @@ if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" -a "${TRAVIS_BRANCH}" == "master" -
     git config --global user.email "Travis CI"
     git config --global user.name "ajay.kannan.15@dartmouth.edu"
     #git clone -b gh-pages https://${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD}@github.com/testuser-aj/temptest.git
-    git clone `git config --get remote.origin.url` 
+    ##git clone `git config --get remote.origin.url` 
+    git clone https://github.com/testuser-aj/temptest.git
     cd temptest
     git checkout gh-pages
     #git branch --set-upstream gh-pages origin/gh-pages
-    git remote set-url origin https://github.com/my_user_name/my_repo.git
+    ##git remote set-url origin https://github.com/my_user_name/my_repo.git
     SITE_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
     SITE_VERSION="$(cut -d '-' -f 1 <<< "$SITE_VERSION")"
     mkdir -p site/latest/
