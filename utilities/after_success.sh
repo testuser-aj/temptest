962d2f854f32130c976f7d8ff48b62f309533f2d
Ajay Kannan <ajaykannan@google.com>
2015-08-22 15:34:10 -0700

    c

--- utilities/after_success.sh
+++ utilities/after_success.sh
@@ -15,20 +15,17 @@ if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" -a "${TRAVIS_BRANCH}" == "master" -
     git config --global user.email ${CI_DEPLOY_USERNAME}@github.com
     git clone https://github.com/testuser-aj/temptest.git
     cd temptest
-    echo $(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
     SITE_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
-    echo $SITE_VERSION
-    SITE_VERSION=${SITE_VERSION%-*}
+    SITE_VERSION=${SITE_VERSION%-*} # Strip "-SNAPSHOT" out of version as necessary
     git checkout gh-pages
     mkdir -p site/latest/
     touch site/latest/index.html
     echo "<html><head><meta http-equiv=\"refresh\" content=\"0; URL='http://googlecloudplatform.github.io/gcloud-java/site/${SITE_VERSION}/index.html'\" /></head><body></body></html>" > site/latest/index.html
     git add site/latest/index.html
-    echo $SITE_VERSION
     sed -i "s/SITE_VERSION/$SITE_VERSION/g" site/${SITE_VERSION}/index.html
     git add site/${SITE_VERSION}/index.html
     git commit -m "Updating to reflect latest website version"
-    git push --force --quiet "https://${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD}@github.com/testuser-aj/temptest.git" origin/gh-pages
+    git push --force "https://${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD}@github.com/testuser-aj/temptest.git" origin/gh-pages
 
     #mvn deploy -DskipTests=true -Dgpg.skip=true --settings target/travis/settings.xml
 else
