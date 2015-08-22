72335caf0ac11c8b0c88c7338a674b6746031eb5
Ajay Kannan <ajaykannan@google.com>
2015-08-21 18:17:12 -0700

    ?

--- utilities/after_success.sh
+++ utilities/after_success.sh
@@ -10,13 +10,9 @@ if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" -a "${TRAVIS_BRANCH}" == "master" -
     echo `git config --get remote.origin.url`
     git config --global user.email "Travis CI"
     git config --global user.name "ajay.kannan.15@dartmouth.edu"
-    #git clone -b gh-pages https://${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD}@github.com/testuser-aj/temptest.git
-    ##git clone `git config --get remote.origin.url` 
     git clone https://github.com/testuser-aj/temptest.git
     cd temptest
     git checkout gh-pages
-    #git branch --set-upstream gh-pages origin/gh-pages
-    ##git remote set-url origin https://github.com/my_user_name/my_repo.git
     SITE_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
     SITE_VERSION="$(cut -d '-' -f 1 <<< "$SITE_VERSION")"
     mkdir -p site/latest/
@@ -24,7 +20,7 @@ if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" -a "${TRAVIS_BRANCH}" == "master" -
     echo "<html><head><meta http-equiv=\"refresh\" content=\"0; URL='http://googlecloudplatform.github.io/gcloud-java/site/$SITE_VERSION />'\"</head><body></body></html>" > site/latest/index.html
     git add site/latest/index.html
     git commit -m "Updating latest website version"
-    git push --force --quiet "https://${GH_TOKEN}@github.com/testuser-aj/temptest.git" origin gh-pages
+    git push --force --quiet "https://${GH_TOKEN}@github.com/testuser-aj/temptest.git" #origin/gh-pages
 
     #mvn cobertura:cobertura coveralls:report
     #mvn site-deploy -DskipTests=true --settings=target/travis/settings.xml
