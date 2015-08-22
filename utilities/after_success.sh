680db4707eec0a8d3f0e877d8306a369333c0a2d
Ajay Kannan <ajaykannan@google.com>
2015-08-22 15:12:29 -0700

    b

--- utilities/after_success.sh
+++ utilities/after_success.sh
@@ -7,27 +7,29 @@ echo "Travis branch:       " ${TRAVIS_BRANCH}
 echo "Travis pull request: " ${TRAVIS_PULL_REQUEST}
 echo "Travis JDK version:  " ${TRAVIS_JDK_VERSION}
 if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" -a "${TRAVIS_BRANCH}" == "master" -a "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-    git config --global user.email "Travis CI"
-    git config --global user.name "ajay.kannan.15@dartmouth.edu"
+    #mvn cobertura:cobertura coveralls:report
+    #mvn site-deploy -DskipTests=true --settings=target/travis/settings.xml
+    
+    # Create/update "latest" html page to redirect to the most recently generated website
+    #git config --global user.email ""
+    git config --global user.name "Travis CI"
     git clone https://github.com/testuser-aj/temptest.git
     cd temptest
-    git checkout gh-pages
     echo $(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
     SITE_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
     echo $SITE_VERSION
     SITE_VERSION=${SITE_VERSION%-*}
+    git checkout gh-pages
     mkdir -p site/latest/
     touch site/latest/index.html
     echo "<html><head><meta http-equiv=\"refresh\" content=\"0; URL='http://googlecloudplatform.github.io/gcloud-java/site/${SITE_VERSION}/index.html'\" /></head><body></body></html>" > site/latest/index.html
     git add site/latest/index.html
     echo $SITE_VERSION
     sed -i "s/SITE_VERSION/$SITE_VERSION/g" site/${SITE_VERSION}/index.html
-    git commit -m "Updating website to reflect latest version"
+    git add site/${SITE_VERSION}/index.html
+    git commit -m "Updating to reflect latest website version"
     git push --force --quiet "https://${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD}@github.com/testuser-aj/temptest.git" origin/gh-pages
-    #git push --force --quiet "https://${GH_TOKEN}@github.com/testuser-aj/temptest.git" origin/gh-pages
 
-    #mvn cobertura:cobertura coveralls:report
-    #mvn site-deploy -DskipTests=true --settings=target/travis/settings.xml
     #mvn deploy -DskipTests=true -Dgpg.skip=true --settings target/travis/settings.xml
 else
     echo "Not deploying artifacts. This is only done with non-pull-request commits to master branch with Oracle Java 7 builds."
