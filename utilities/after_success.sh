db216b9e1fd9aa8ec78925a764bebdb39557101c
Ajay Kannan <ajaykannan@google.com>
2015-08-21 18:23:23 -0700

    ?

--- utilities/after_success.sh
+++ utilities/after_success.sh
@@ -11,6 +11,7 @@ if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" -a "${TRAVIS_BRANCH}" == "master" -
     git config --global user.email "Travis CI"
     git config --global user.name "ajay.kannan.15@dartmouth.edu"
     git clone https://github.com/testuser-aj/temptest.git
+    #git config --global push.default matching
     cd temptest
     git checkout gh-pages
     SITE_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
@@ -20,7 +21,7 @@ if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" -a "${TRAVIS_BRANCH}" == "master" -
     echo "<html><head><meta http-equiv=\"refresh\" content=\"0; URL='http://googlecloudplatform.github.io/gcloud-java/site/$SITE_VERSION />'\"</head><body></body></html>" > site/latest/index.html
     git add site/latest/index.html
     git commit -m "Updating latest website version"
-    git push --force --quiet "https://${GH_TOKEN}@github.com/testuser-aj/temptest.git" #origin/gh-pages
+    git push --force --quiet "https://${GH_TOKEN}@github.com/testuser-aj/temptest.git" origin/gh-pages
 
     #mvn cobertura:cobertura coveralls:report
     #mvn site-deploy -DskipTests=true --settings=target/travis/settings.xml
