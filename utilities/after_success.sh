a7775b4814458687ac73296955ad9ca0b935593e
Ajay Kannan <ajaykannan@google.com>
2015-08-24 13:40:48 -0700

    test updates to travis in experimental copy

--- utilities/after_success.sh
+++ utilities/after_success.sh
@@ -7,29 +7,32 @@ echo "Travis branch:       " ${TRAVIS_BRANCH}
 echo "Travis pull request: " ${TRAVIS_PULL_REQUEST}
 echo "Travis JDK version:  " ${TRAVIS_JDK_VERSION}
 if [ "${TRAVIS_JDK_VERSION}" == "oraclejdk7" -a "${TRAVIS_BRANCH}" == "master" -a "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-    #mvn cobertura:cobertura coveralls:report
-    mvn site-deploy -DskipTests=true --settings=target/travis/settings.xml
-    
-    # Create/update "latest" html page to redirect to the most recently generated website
-    git config --global user.name "Travis CI"
-    git config --global user.email ${CI_DEPLOY_USERNAME}@github.com
-    git clone https://github.com/testuser-aj/temptest.git #######################################
-    cd temptest #################################################################################
-    SITE_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
-    SITE_VERSION=${SITE_VERSION%-*} # Strip "-SNAPSHOT" out of version as necessary
-    SITE_VERSION=0.0.6 ###########################################################################
-    git checkout gh-pages
-    mkdir -p site/latest/
-    touch site/latest/index.html
-    echo "<html><head><meta http-equiv=\"refresh\" content=\"0; URL='http://googlecloudplatform.github.io/gcloud-java/site/${SITE_VERSION}/index.html'\" /></head><body></body></html>" > site/latest/index.html
-    git add site/latest/index.html
-    sed -i "s/SITE_VERSION/$SITE_VERSION/g" site/${SITE_VERSION}/index.html
-    git add site/${SITE_VERSION}/index.html
-    git commit -m "Updating to reflect latest website version"
-    git config --global push.default simple
-    git push --force "https://${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD}@github.com/testuser-aj/temptest.git"
-
+    mvn cobertura:cobertura coveralls:report
     #mvn deploy -DskipTests=true -Dgpg.skip=true --settings target/travis/settings.xml
+
+    # Deploy site if not a SNAPSHOT
+    SITE_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -Ev '(^\[|Download\w+:)')
+    if [ "${SITE_VERSION##*-}" != "SNAPSHOT" ]; then
+        #mvn site-deploy -DskipTests=true --settings=target/travis/settings.xml
+
+        # Update "latest" webpage
+        git config --global user.name "travis-ci"
+        git config --global user.email "travis@travis-ci.org"
+        git clone --branch gh-pages --single-branch https://github.com/testuser-aj/temptest/ tmp_gh-pages
+        cd tmp_gh-pages
+        mkdir -p site/latest/
+        touch site/latest/index.html
+        echo "<html><head><meta http-equiv=\"refresh\" content=\"0; URL='http://GoogleCloudPlatform.github.io/gcloud-java/site/${SITE_VERSION}/index.html'\" /></head><body></body></html>" > site/latest/index.html
+        git add site/latest/index.html
+
+        # Update "Quickstart with Maven" block on landing page to reflect latest version
+        sed -i "s/{{SITE_VERSION}}/$SITE_VERSION/g" site/${SITE_VERSION}/index.html
+        git add site/${SITE_VERSION}/index.html
+
+        git commit -m "Updating to reflect latest website version"
+        git config --global push.default simple
+        git push --quiet "https://${CI_DEPLOY_USERNAME}:${CI_DEPLOY_PASSWORD}@github.com/testuser-aj/temptest.git" > /dev/null 2>&1
+    fi
 else
     echo "Not deploying artifacts. This is only done with non-pull-request commits to master branch with Oracle Java 7 builds."
 fi
